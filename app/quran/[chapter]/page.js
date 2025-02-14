4029167493d100b38b23ff1e8f273d4322ffb52e
Riad <riad_mammadov@yahoo.com>
2025-02-14 20:18:31 +0000

    add loader

--- app/quran/[chapter]/page.js
+++ app/quran/[chapter]/page.js
@@ -1,10 +1,10 @@
 "use client";
 
 import { useState, useEffect, useRef } from "react";
-import Button from "@mui/material/Button";
 
 import { useParams } from "next/navigation";
 import { surahs } from "@/utils/surahs";
+import { CircularProgress } from "@mui/material";
 
 function ChapterPage() {
   const params = useParams();
@@ -28,47 +28,52 @@ function ChapterPage() {
   if (!data)
     return (
       <div className="min-h-screen bg-gray-900 text-white flex items-center justify-center">
-        Loading...
+        <CircularProgress color="inherit" />
       </div>
     );
 
-  const scrollToVerse = (index) => {
-    verseRefs.current[index]?.scrollIntoView({
-      behavior: "smooth",
-      block: "start",
-    });
-  };
+  // const scrollToVerse = (index) => {
+  //   verseRefs.current[index]?.scrollIntoView({
+  //     behavior: "smooth",
+  //     block: "start",
+  //   });
+  // };
 
   return (
     <div className="min-h-screen bg-gray-900 text-gray-100">
       <main className="container mx-auto px-4 py-8">
+        <aside className="fixed mt-52">
+          <h1>Sidebar stuff here</h1>
+        </aside>
         <div className="mb-8 text-center">
           <h1 className="text-2xl mt-20 font-bold">
             {surahs[chapter - 1].title}
           </h1>
         </div>
-        <div className="space-y-12">
-          {data.arabic1.map((verse, index) => (
-            <div
-              key={index}
-              ref={(el) => (verseRefs.current[index] = el)}
-              className="bg-gray-800 rounded-lg p-6 shadow-lg transition-all duration-300 hover:shadow-xl"
-            >
-              <div className="flex flex-col">
-                <p className="text-3xl lg:text-4xl tracking-wider mb-6 text-right font-arabic">
-                  {verse}
-                </p>
-                <div className="lg:w-full lg:pr-8">
-                  <p className="text-base lg:text-lg text-gray-300 mb-4">
-                    {data.english[index]}
+        <div className="flex flex-col items-end">
+          <div className="space-y-12 w-2/3">
+            {data.arabic1.map((verse, index) => (
+              <div
+                key={index}
+                ref={(el) => (verseRefs.current[index] = el)}
+                className="bg-gray-800 rounded-lg p-6 shadow-lg transition-all duration-300 hover:shadow-xl"
+              >
+                <div className="flex flex-col">
+                  <p className="text-3xl lg:text-4xl tracking-wider mb-6 text-right font-arabic">
+                    {verse}
                   </p>
-                  <div className="text-sm text-gray-400 text-right">
-                    Verse {index + 1}
+                  <div className="lg:w-full lg:pr-8">
+                    <p className="text-base lg:text-lg text-gray-300 mb-4">
+                      {data.english[index]}
+                    </p>
+                    <div className="text-sm text-gray-400 text-right">
+                      Verse {index + 1}
+                    </div>
                   </div>
                 </div>
               </div>
-            </div>
-          ))}
+            ))}
+          </div>
         </div>
       </main>
     </div>
