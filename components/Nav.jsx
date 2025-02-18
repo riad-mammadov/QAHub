25cefbe12c850d243b8ab3bec56ce78b98c82106
Riad <riad_mammadov@yahoo.com>
2025-02-18 22:05:51 +0000

    implement hide on scroll feature

--- components/Nav.jsx
+++ components/Nav.jsx
@@ -5,12 +5,34 @@ import Link from "next/link";
 import { navLinks } from "@/utils/links";
 import { useState } from "react";
 import MobileNavbar from "./MobileNavbar";
+import { motion, useScroll, useMotionValueEvent } from "framer-motion";
 
 function Nav() {
   const [isMenuOpen, setIsMenuOpen] = useState(false);
 
+  const { scrollY } = useScroll();
+
+  const [hidden, setHidden] = useState(false);
+
+  useMotionValueEvent(scrollY, "change", (latest) => {
+    const prev = scrollY.getPrevious();
+    if (latest > prev && latest > 50) {
+      setHidden(true);
+    } else {
+      setHidden(false);
+    }
+  });
+
   return (
-    <div className="absolute top-2 left-0 right-0 flex justify-between items-center z-50 bg-transparent p-4 px-12">
+    <motion.div
+      variants={{
+        visible: { y: -10 },
+        hidden: { y: "-130%" },
+      }}
+      animate={hidden ? "hidden" : "visible"}
+      transition={{ duration: 0.35, ease: "easeInOut" }}
+      className="sticky bg-gray-900/90 top-2 left-0 right-0 flex justify-between items-center z-50 bg-none p-4 px-12"
+    >
       <Link href="/">
         <button className="text-stone-300 text-2xl font-semibold font-serif tracking-wider px-5 py-2.5">
           QAHub
@@ -50,7 +72,7 @@ function Nav() {
           links={navLinks}
         />
       )}
-    </div>
+    </motion.div>
   );
 }
 
