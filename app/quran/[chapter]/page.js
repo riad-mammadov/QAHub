29a19fb3577c9d11c68182b8123989749e2c1f03
Riad <riad_mammadov@yahoo.com>
2025-02-19 17:13:40 +0000

    improve layout

--- app/quran/[chapter]/page.js
+++ app/quran/[chapter]/page.js
@@ -42,7 +42,7 @@ function ChapterPage() {
   // };
 
   return (
-    <div className="min-h-screen bg-gradient-to-b from-gray-900 to-gray-800 text-gray-100 py-8 px-4 sm:px-6 lg:px-8">
+    <div className="min-h-screen bg-gradient-to-b from-gray-900 to-gray-800 text-gray-100 py-2 px-4 sm:px-6 lg:px-8">
       <div className="max-w-4xl mx-auto">
         <header className="flex justify-center items-center mb-8">
           <h1 className="text-3xl font-bold text-center">
