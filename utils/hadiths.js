4adbd38715d79ea60e3e1b192b6a670aa1e8ee08
Riad <riad_mammadov@yahoo.com>
2025-03-10 14:27:11 +0000

    add hadith collections

---
+++ utils/hadiths.js
@@ -0,0 +1,11 @@
+export const hadiths = [
+  { title: "Sahih Bukhari", slug: "sahih-bukhari", id: 1 },
+  { title: "Sahih Muslim", slug: "sahih-muslim", id: 2 },
+  { title: "Jami' Al-Tirmidhi", slug: "al-tirmidhi", id: 3 },
+  { title: "Sunan Abu Dawood", slug: "abu-dawood", id: 4 },
+  { title: "Sunan Ibn-e-Majah", slug: "ibn-e-majah", id: 5 },
+  { title: "Sunan An-Nasa`i", slug: "sunan-nasai", id: 6 },
+  { title: "Mishkat Al-Masabih", slug: "mishkat", id: 7 },
+  { title: "Musnad Ahmad", slug: "musnad-ahmad", id: 8 },
+  { title: "Al-Silsila Sahiha", slug: "al-silsila-sahiha", id: 9 },
+];
